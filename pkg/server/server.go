5c6d075834879fc6aede6167db2ad612fee4d733
Grzegorz Płaczek <g01.placzek@gmail.com>
2024-05-05 18:58:02 +0200

    Handle POST for events

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,9 +1,9 @@
 package server
 
 import (
-	"time"
-	"net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
 type Event struct {
@@ -13,16 +13,36 @@ type Event struct {
 	Date        time.Time `json:"date"`
 }
 
-var events = []Event{
-	{ID: 1, Name: "Wywalić śmiecie", Description: "", Date: time.Date(2024, time.May, 5, 10, 0, 0, 0, time.UTC)},
-}
+var (
+	events = []Event{
+		{ID: 1, Name: "Wywalić śmiecie", Description: "", Date: time.Date(2024, time.May, 5, 10, 0, 0, 0, time.UTC)},
+	}
+	evId uint64 = 2
+)
 
 func getEvents(c *gin.Context) {
 	c.JSON(http.StatusOK, events)
 }
 
+func addEvent(c *gin.Context) {
+	var ev Event
+
+	if err := c.BindJSON(&ev); err != nil {
+		c.Status(http.StatusUnprocessableEntity)
+		return
+	}
+
+	ev.ID = evId
+	evId++
+
+	events = append(events, ev)
+
+	c.JSON(http.StatusOK, gin.H{"id": ev.ID})
+}
+
 func RunServer() {
 	router := gin.Default()
 	router.GET("/events", getEvents)
+	router.POST("/events", addEvent)
 	router.Run("localhost:8080")
 }
