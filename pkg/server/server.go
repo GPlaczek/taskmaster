e4577bf33205123cb54860659a20714d4472f3c0
Grzegorz Płaczek <g01.placzek@gmail.com>
2024-05-05 20:08:41 +0200

    Implement GET for /events/:id

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -40,9 +41,33 @@ func addEvent(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"id": ev.ID})
 }
 
+func getEvent(c *gin.Context) {
+	_id := c.Param("id")
+	id, err := strconv.ParseUint(_id, 10, 64)
+	if err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	var event *Event = nil
+	for i, _ := range events {
+		if events[i].ID == id {
+			event = &events[i]
+		}
+	}
+
+	if event == nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+
+	c.JSON(http.StatusOK, event)
+}
+
 func RunServer() {
 	router := gin.Default()
 	router.GET("/events", getEvents)
 	router.POST("/events", addEvent)
+	router.GET("/events/:id", getEvent)
 	router.Run("localhost:8080")
 }
